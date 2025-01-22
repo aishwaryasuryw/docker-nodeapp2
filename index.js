1da584ca2e0a1c0a9c8c8e489169d2d870ddbb6a
aish <aish@email.com>
2025-01-23 00:12:31 +0530

    sj

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('khao pio aish karo mitro ');
+ res.send('khao pio aish karo mitro.. ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
